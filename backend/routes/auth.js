fd85befe86666ea2ec5e5065bba4fac6ae16d75a
imvbhargav <87763304+imvbhargav@users.noreply.github.com>
2025-04-05 19:06:16 +0530

    sameSite removed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,7 +47,7 @@ router.post("/login", async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: "None",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
