a4cf9ce93d1946ed69e41f3fd8d57dc3c65a9661
imvbhargav <87763304+imvbhargav@users.noreply.github.com>
2025-04-05 18:51:05 +0530

    shifted backed to koyeb

--- frontend/bookstoreui/src/assets/options.js
+++ frontend/bookstoreui/src/assets/options.js
@@ -2,6 +2,6 @@ const LIMIT_PER_PAGE = 12;
 const DEV_BACKEND = "http://localhost:5000";
 const KOYEB_URL = "https://slim-bonita-bhargavv-5a037565.koyeb.app";
 const RENDER_BACKEND = "https://bookstore-cgas.onrender.com";
-const BACKEND_URL = RENDER_BACKEND;
+const BACKEND_URL = KOYEB_URL;
 
 export { LIMIT_PER_PAGE, DEV_BACKEND, BACKEND_URL };
