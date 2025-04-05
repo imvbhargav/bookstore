6c00858be83ab19da45145c222fb60317b2b40da
imvbhargav <87763304+imvbhargav@users.noreply.github.com>
2025-04-05 18:22:55 +0530

    Added working backend links

--- frontend/bookstoreui/src/assets/options.js
+++ frontend/bookstoreui/src/assets/options.js
@@ -1,6 +1,6 @@
 const LIMIT_PER_PAGE = 12;
 const DEV_BACKEND = "http://localhost:5000";
-const RENDER_BACKEND = "https://";
-const KOYEB_BACKEND = "https://";
+const RENDER_BACKEND = "https://bookstore-cgas.onrender.com";
+const KOYEB_BACKEND = "https://slim-bonita-bhargavv-5a037565.koyeb.app";
 
 export { LIMIT_PER_PAGE, DEV_BACKEND, RENDER_BACKEND, KOYEB_BACKEND };
